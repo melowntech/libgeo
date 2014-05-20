4652ceb5d6fb645b6254a79b66481718181e1139
Ladislav Horký <ladislav.horky@citationtech.net>
2014-05-20 11:47:30 +0000

    geo: added readSrs.
    git-svn-id: https://scm.citationtech.net/trunk/shared/geo/geo@10028 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- geo/io.hpp
+++ geo/io.hpp
@@ -3,6 +3,8 @@
 
 #include "srsdef.hpp"
 #include <fstream>
+#include "utility/streams.hpp"
+#include "dbglog/dbglog.hpp"
 
 namespace geo {
 
@@ -10,10 +12,10 @@ namespace geo {
  * @brief Writes srs to a given file.
  * @detail Srs is saved in format specified in 'type' .
  */
-void writeSrs( const boost::filesystem::path &path
-             , const SrsDefinition & srs
-             , const SrsDefinition::Type &type
-                    = SrsDefinition::Type::proj4 )
+inline void writeSrs( const boost::filesystem::path &path
+                    , const SrsDefinition & srs
+                    , const SrsDefinition::Type &type
+                        = SrsDefinition::Type::wkt )
 {
     std::ofstream f( path.native() );
 
@@ -23,13 +25,31 @@ void writeSrs( const boost::filesystem::path &path
 }
 
 /**
+ * @brief Reads srs from file
+ * @detail The content of file is expected to have type 'type'
+ */
+inline geo::SrsDefinition readSrs( const boost::filesystem::path &path
+                                 , const SrsDefinition::Type &type
+                                        = SrsDefinition::Type::wkt )
+{
+    try {
+        return { utility::read(path), type };
+    } catch (const std::exception &e) {
+        LOGTHROW(err1, std::runtime_error)
+            << "Unable to read srs file " << path << ": " << e.what()
+            << ".";
+    }
+    throw;
+}
+
+/**
  * @brief Saves transformation from image coordinates to world coordinates.
  * @detail PixelSize is in map units (meters), therefore height
  *         is usually negative.
  */
-void writeTfw( const boost::filesystem::path &path
-             , const math::Extents2 &extents
-             , const math::Size2f &pixelSize = math::Size2f(1.0,-1.0) )
+inline void writeTfw( const boost::filesystem::path &path
+                    , const math::Extents2 &extents
+                    , const math::Size2f &pixelSize = math::Size2f(1.0,-1.0) )
 {
     std::ofstream tfw( path.native() );
     double a = pixelSize.width, e = pixelSize.height,
