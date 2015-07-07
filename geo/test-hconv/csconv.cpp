72bc9ff7d0b4736af96603aece334a853665e251
Václav Blažek <vaclav.blazek@citationtech.net>
2015-07-07 10:01:03 +0000

    shared/geo: added csconv (handy CS convertor with proj4/wkt/epsg src/dst SRS definition)
    git-svn-id: https://scm.citationtech.net/trunk/shared/geo/geo@13834 b7e6147c-0f76-4c84-a09d-19069afaf10c

---
+++ geo/test-hconv/csconv.cpp
@@ -0,0 +1,95 @@
+#include <cstdlib>
+#include <string>
+#include <iostream>
+
+#include "dbglog/dbglog.hpp"
+
+#include "service/cmdline.hpp"
+#include "utility/gccversion.hpp"
+#include "utility/buildsys.hpp"
+
+#include "geo/srsdef.hpp"
+#include "geo/csconvertor.hpp"
+
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
+
+namespace {
+
+class CsSonv : public service::Cmdline
+{
+public:
+    CsSonv()
+        : Cmdline("csconv", BUILD_TARGET_VERSION)
+    {
+    }
+
+private:
+    virtual void configuration(po::options_description &cmdline
+                               , po::options_description &config
+                               , po::positional_options_description &pd)
+        UTILITY_OVERRIDE;
+
+    virtual void configure(const po::variables_map&) UTILITY_OVERRIDE {}
+
+    virtual bool help(std::ostream &out, const std::string &what) const
+        UTILITY_OVERRIDE;
+
+    virtual int run() UTILITY_OVERRIDE;
+
+    geo::SrsDefinition src_;
+    geo::SrsDefinition dst_;
+};
+
+void CsSonv::configuration(po::options_description &cmdline
+                           , po::options_description &config
+                           , po::positional_options_description &pd)
+{
+    cmdline.add_options()
+        ("src", po::value(&src_)->required()
+         , "Source SRS definition.")
+        ("dst", po::value(&dst_)->required()
+         , "Destination SRS definition.")
+    ;
+
+    pd
+        .add("src", 1)
+        .add("dst", 1)
+        ;
+
+    (void) config;
+}
+
+bool CsSonv::help(std::ostream &out, const std::string &what) const
+{
+    if (what.empty()) {
+        out << R"RAW(csconv: coordinate system to coordinate system  convertor
+usage
+    csconv src-SRS dst-SRS [point point ...]
+
+    Points are read from stdin in format x y.
+)RAW";
+    }
+    return false;
+}
+
+int CsSonv::run()
+{
+    geo::CsConvertor cs(src_, dst_);
+
+    std::cout << std::setprecision(15);
+    math::Point2 p;
+    while (std::cin >> p(0) >> p(1)) {
+        auto p2(cs(p));
+        std::cout << p2(0) << ' ' << p2(1) << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+} // namespace
+
+int main(int argc, char *argv[])
+{
+    return CsSonv()(argc, argv);
+}
