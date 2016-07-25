b2a436d618f1d9461b1f968115560deaf79004b0
Vaclav Blazek <vaclav.blazek@citationtech.net>
2016-07-25 18:21:54 +0200

    added height coding skeleton

---
+++ geo/heightcoding.cpp
@@ -0,0 +1,37 @@
+#include "dbglog/dbglog.hpp"
+
+#include "jsoncpp/json.hpp"
+
+#include "./heightcoding.hpp"
+
+namespace geo {
+
+void heightCode(::GDALDataset &vectorDs
+                , const GeoDataset &rasterDs
+                , std::ostream &os
+                , const HeightCodingConfig &config)
+{
+    // TODO: magic happens here
+    (void) vectorDs;
+    (void) rasterDs;
+
+    // output
+    switch (config.format) {
+    case HeightCodingConfig::Format::geodataJson: {
+        Json::Value output(Json::objectValue);
+
+        // TODO: fill output from geometries here
+
+        os.precision(15);
+        Json::StyledStreamWriter().write(os, output);
+        break;
+    }
+
+    default:
+        // unsupported
+        LOGTHROW(err1, std::runtime_error)
+            << "Unsupported output vector format <" << config.format << ">.";
+    }
+}
+
+} // namespace geo
---
+++ geo/heightcoding.hpp
@@ -0,0 +1,67 @@
+#ifndef geo_heightcoding_hpp_included_
+#define geo_heightcoding_hpp_included_
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include <boost/optional.hpp>
+
+#include "utility/enum-io.hpp"
+
+#include "./geodataset.hpp"
+
+namespace geo {
+
+/** Height coding.
+ */
+struct HeightCodingConfig {
+    enum class Format { geodataJson };
+
+    /** Working spatial reference system. Input data in vector dataset) are
+     * expected to be in this dataset. Vector Dataset should have vertical
+     * component the same as the data in input raster (although GDAL dataset
+     * lacks any notion of vertical system).
+     *
+     * Raster datasets's SRS is used if invalid.
+     */
+    boost::optional<SrsDefinition> workingSrs;
+
+    /** Spatial reference system of generated output. No conversion is performed
+     *  if invalid.
+     */
+    boost::optional<SrsDefinition> outputSrs;
+
+    /** List of layer names; none -> all layers present in input dataset
+     */
+    boost::optional<std::vector<std::string>> layers;
+
+    /** Output format.
+     */
+    Format format;
+
+    HeightCodingConfig() : format(Format::geodataJson) {}
+};
+
+/** Height code vector data from vectorDs using height information from
+ *  raster dataset rasterDs.
+ *
+ *  \param vectorDs input vector dataset
+ *  \param rasterDs raster dataset used to height code vector data
+ *  \param os output stream result is written to
+ *  \param config work configuration
+ */
+void heightCode(::GDALDataset &vectorDs, const GeoDataset &rasterDs
+                , std::ostream &os
+                , const HeightCodingConfig &config = HeightCodingConfig());
+
+
+// inlines
+
+UTILITY_GENERATE_ENUM_IO(HeightCodingConfig::Format,
+                      ((geodataJson))
+                      )
+
+} // namespace geo
+
+#endif // geo_heightcoding_hpp_included_
